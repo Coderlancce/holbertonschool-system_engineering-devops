edc117351abe762723951da47affdcf0d03f281d
Coderlancce <coderlancce@gmail.com>
2022-09-09 09:32:35 -0500

    [OK] advanced proyect

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 def recurse(subreddit, hot_list=[], after=""):
     """
-    Returns a list containing the titles of all hot articles for a given
+    Returns list containing the titles of all hot articles for a given
     subreddit. If no results are found for the given subreddit, the function
     should return None.
     """
